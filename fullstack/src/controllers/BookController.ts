2f04cc2b6bd7ea55cb85fa33879723ca1f40d9ba
Manco <santiagomancomaya@gmail.com>
2026-02-10 17:26:01 -0500

    Additional changes

--- fullstack/src/controllers/BookController.ts
+++ fullstack/src/controllers/BookController.ts
@@ -4,18 +4,18 @@ import { Book } from '../models/Book.js';
 
 export class BookController {
 
-    static index(req: Request, res: Response): void {
-      const viewData: any = {};
-      viewData["books"] = books;
+  static index(req: Request, res: Response): void {
+    const viewData: any = {};
+    viewData["books"] = books;
 
-      res.render('books/index', viewData);
-    }
+    res.render('books/index', viewData);
+  }
 
-    static show(req: Request, res: Response): void {
-      const book = Book.findById(books, parseInt(req.params.id as string));
-      const viewData: any = {};
-      viewData["book"] = book;
+  static show(req: Request, res: Response): void {
+    const book = Book.findById(books, parseInt(req.params.id as string));
+    const viewData: any = {};
+    viewData["book"] = book;
 
-      res.render('books/show', viewData);
-    }
+    res.render('books/show', viewData);
+  }
 }
